f1461fe3ac2ffb57fcffa9b3e314af3df8b17215
Richard Corrente <90164856+ExzoZbta@users.noreply.github.com>
2023-12-03 03:02:23 -0500

    added print statements, set fixed runtime to make it easier to follow state of movement

--- arduino/actuator.ino
+++ arduino/actuator.ino
@@ -1,21 +1,35 @@
 #include <Servo.h>
 
-Servo myservo;
+Servo motor1;
 
 void setup() {
-  myservo.attach(10);
+  Serial.begin(9600);
+  motor1.attach(21);
 }
 
+int run = 0;
+
 void loop() {
-  myservo.write(180); // move actuator forward
-  delay(800); // wait for actuator to finish
-  myservo.write(90); // make actuator stop
+  
+  // run 5 times
+  if (run < 5) {
+    motor1.write(180); // move actuator forward
+    Serial.println("forward");
+    delay(800); // wait for actuator to finish
+    motor1.write(90); // make actuator stop
+    Serial.println("stop");
 
-  delay(500); // keep actuator in place
+    delay(500); // keep actuator in place
+    Serial.println("wait");
 
-  myservo.write(0); // move actuator backward
-  delay(800); // wait for actuator to finish
-  myservo.write(90); // make actuator stop
+    motor1.write(0); // move actuator backward
+    Serial.println("backward");
+    delay(800); // wait for actuator to finish
+    motor1.write(90); // make actuator stop
+    Serial.println("stop");
 
-  // while(1) ()
+    run++;
+  }
+
+  
 }
