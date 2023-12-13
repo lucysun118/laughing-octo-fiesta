65429af78767be86c5b879ad62df60c2e5a41a30
Lucy Sun <l.sun@yale.edu>
2023-12-13 12:13:28 -0500

    modified code to parse through Serial

--- serial_read/test_serialread.py
+++ serial_read/test_serialread.py
@@ -1,21 +1,17 @@
-"""
-Usage example: 
-
-Run file with terminal command:
-/Users/lucysun/laughing-octo-fiesta/serial_read/musenet_code/text_to_midi.py -e 7 -n 3 -m 8 -t 5 -u 6 -r 4 -j 9
-
-"""
-
 import time
 import serial
 import subprocess
-import webbrowser  # Import the webbrowser module
+import webbrowser
 from scale_vals import scale_values
 from musenet_code import text_to_midi
 
 ser = serial.Serial(port="/dev/tty.usbserial-110", baudrate=115200, timeout=5)
 
 time.sleep(0.5)
+
+# Initialize variables
+e, n, m, t, u, r, j = 0, 0, 0, 0, 0, 0, 0
+command_executed = False
 
 def read_line():
     try:
@@ -28,17 +24,45 @@ while True:
     elapsed_time_val = read_line()
     print(f"raw val: {elapsed_time_val}")
 
-    scaled_val = scale_values(elapsed_time_val)
-    print(f"scaled val: {scaled_val}")
+    # Check the first two characters of the incoming serial data
+    prefix = elapsed_time_val[:2]
 
-    # Run the other script with the scaled value as an argument
-    subprocess.run(["/usr/local/bin/python3", "/Users/lucysun/laughing-octo-fiesta/serial_read/musenet_code/text_to_midi.py", "-e", str(scaled_val)])
+    # Assign values based on the prefix
+    if prefix == "e:":
+        e = int(elapsed_time_val[2:])
+    elif prefix == "n:":
+        n = int(elapsed_time_val[2:])
+    elif prefix == "m:":
+        m = int(elapsed_time_val[2:])
+    elif prefix == "t:":
+        t = int(elapsed_time_val[2:])
+    elif prefix == "u:":
+        u = int(elapsed_time_val[2:])
+    elif prefix == "r:":
+        r = int(elapsed_time_val[2:])
+    elif prefix == "j:":
+        j = int(elapsed_time_val[2:])
 
-    # Open the HTML page with the generated MIDI file in a web browser
-    webbrowser.open('midi_notes.html', new=2)
+    print(f"e: {e}, n: {n}, m: {m}, t: {t}, u: {u}, r: {r}, j: {j}")
+
+    # Check if all variables are set
+    if all((e, n, m, t, u, r, j)):
+        # Run the command with the stored values
+        command = ["/Users/lucysun/laughing-octo-fiesta/serial_read/musenet_code/text_to_midi.py",
+                   "-e", str(e), "-n", str(n), "-m", str(m), "-t", str(t), "-u", str(u), "-r", str(r), "-j", str(j)]
+
+        subprocess.run(["/usr/local/bin/python3"] + command)
+
+        # Open the HTML page with the generated MIDI file in a web browser
+        webbrowser.open('midi_notes.html', new=2)
+
+        # Reset variables
+        e, n, m, t, u, r, j = 0, 0, 0, 0, 0, 0, 0
+        command_executed = True
+
+    elif command_executed:
+        # Reset variables if the command has been executed and not all variables are set
+        e, n, m, t, u, r, j = 0, 0, 0, 0, 0, 0, 0
+        command_executed = False
 
 ser.close()
-
-
-
-
