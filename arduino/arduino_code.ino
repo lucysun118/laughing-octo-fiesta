84a665e66fd3b68e59e23f3b68e2784c1d354e09
Lucy Sun <l.sun@yale.edu>
2023-12-13 14:14:59 -0500

    modified arduino code to work correctly for motors and led and send correct data over serial

--- arduino/arduino_code.ino
+++ arduino/arduino_code.ino
@@ -7,7 +7,7 @@ Servo motor3;
 Servo motor4;
 Servo motor5;
 Servo motor6;
-ezButton button(21); // Define a button on GPIO pin 21
+ezButton button(22); // Define a button on GPIO pin 21
 ezButton mySwitch(13); // create ezButton object that attaches to ESP32 pin GPIO16
 
 int motor1_pos = 0;
@@ -29,16 +29,18 @@ int curr_elapsed_time = 0;
 int switch_state;
 
 // LED pin
-const int ledPin = 3;
+#define ledRedPin   12 // GPIO23
+#define ledGreenPin  14 // GPIO22
+#define ledBluePin   27 // GPIO21
 
 void setup() {
   Serial.begin(115200);
-  motor1.attach(5);
-  motor2.attach(6);
-  motor3.attach(7);
-  motor4.attach(8);
-  motor5.attach(9);
-  motor6.attach(10);
+  motor1.attach(15);
+  motor2.attach(2);
+  motor3.attach(0);
+  motor4.attach(4);
+  motor5.attach(16);
+  motor6.attach(17);
 
   button.setDebounceTime(50); // Set debounce time to 50 ms
   mySwitch.setDebounceTime(50); // set debounce time to 50 milliseconds
@@ -48,38 +50,95 @@ void setup() {
   pinMode(ledBluePin, OUTPUT);
 }
 
-void move_servo_forward(Servo &motor) {
+void move_servo_forward(int state, Servo &motor) {
+  button.loop();
   mySwitch.loop();
-  switch_state = mySwitch.getState();
 
-  if ((curr_elapsed_time >= 7000) || (switch_state == HIGH)) {
+  switch_state = mySwitch.getState();
+  //Serial.println(switch_state);
+
+   //Serial.print("moving forwards");
+  // Serial.print(curr_elapsed_time);
+  // Serial.print("state:");
+  // Serial.println(state);
+  // Serial.print(", switch pos:");
+  // Serial.println(switch_state);
+
+  if (button.isPressed()) {
+    button_pressed_state = (button_pressed_state + 1) % 18;
+    //Serial.print("button: " + button_pressed_state);
+    motor_state = button_pressed_state / 2;
+    // Serial.print("motor state: ");
+    // Serial.println(motor_state);
+  }
+
+  if ((motor_state != state) || (curr_elapsed_time >= 7000) || (switch_state == HIGH)) {
+    //Serial.print("state changed while moving forward");
     return;
   } else {
     motor.write(95); // Full speed in the opposite direction
     curr_elapsed_time = curr_elapsed_time + 200;
     delay(200);
-    move_servo_forward(motor);
+    move_servo_forward(state, motor);
   }
 }
 
-void move_servo_backward(Servo &motor) {
-  mySwitch.loop();
-  switch_state = mySwitch.getState();
+void move_servo_backward(int state, Servo &motor) {
 
-  if ((curr_elapsed_time <= -7000) || (switch_state == LOW)) {
+  button.loop();
+  mySwitch.loop();
+
+  switch_state = mySwitch.getState();
+  //Serial.println(switch_state);
+
+  //Serial.print("moving backwards");
+  // Serial.print(curr_elapsed_time);
+  // Serial.print("state:");
+  // Serial.println(state);
+  // Serial.print(", switch pos:");
+  // Serial.println(switch_state);
+
+  if (button.isPressed()) {
+    button_pressed_state = (button_pressed_state + 1) % 18;
+    //Serial.println("button: " + button_pressed_state);
+    motor_state = button_pressed_state / 2;
+    // Serial.print("motor state: ");
+    // Serial.println(motor_state);
+  }
+
+  if ((motor_state != state) || (curr_elapsed_time <= -7000) || (switch_state == LOW)) {
+    //Serial.print("state changed while moving backward");
     return;
   } else {
-    motor.write(87); // Full speed in the opposite direction
+    motor.write(83); // Full speed in the opposite direction
     curr_elapsed_time = curr_elapsed_time - 200;
     delay(200);
-    move_servo_backward(motor);
+    move_servo_backward(state, motor);
   }
 }
 
-void stop_servo(Servo &motor) {
+void stop_servo(int state, Servo &motor) {
+  button.loop();
+  mySwitch.loop();
+
   switch_state = mySwitch.getState();
+  //Serial.print("motor stopped");
+  //Serial.println(switch_state);
+
+  if (button.isPressed()) {
+    button_pressed_state = (button_pressed_state + 1) % 18;
+    //Serial.println("button: " + button_pressed_state);
+    motor_state = button_pressed_state / 2;
+    // Serial.print("motor state: ");
+    // Serial.println(motor_state);
+  }
+  if (state != motor_state) {
+    //Serial.print("state changed while stopped");
+    return;
+  }
   motor.write(90); // Stop the servo
   delay(1000);
+  //stop_servo(motor_state, motor);
 }
 
 void setRGB(int red, int green, int blue) {
@@ -93,218 +152,259 @@ void loop() {
   mySwitch.loop();
 
   switch_state = mySwitch.getState();
+  //Serial.println(switch_state);
 
   if (button.isPressed()) {
     button_pressed_state = (button_pressed_state + 1) % 18;
+    //Serial.println("button: " + button_pressed_state);
     motor_state = button_pressed_state / 2;
 
     // Stop all motors and turn off the LED
-    motor1.write(90);
-    motor2.write(90);
-    motor3.write(90);
-    motor4.write(90);
-    motor5.write(90);
-    motor6.write(90);
-    setRGB(0, 0, 0); // Turn off the RGB LED
+    // motor1.write(90);
+    // motor2.write(90);
+    // motor3.write(90);
+    // motor4.write(90);
+    // motor5.write(90);
+    // motor6.write(90);
+    // setRGB(0, 0, 0); // Turn off the RGB LED
+  }
+  // Serial.print("motor state: ");
+  // Serial.println(motor_state);
 
-    switch (motor_state) {
-      case 1:
-        if ((curr_elapsed_time <= -6800) || (switch_state == LOW)) {
-          curr_elapsed_time = 0;
-          move_servo_forward(motor1);
-        }
-        // Stop the servo
-        while ((switch_state == LOW) && (curr_elapsed_time >= 6800)) {
-          mySwitch.loop();
-          switch_state = mySwitch.getState();
-          stop_servo(motor1);
-        }
-
-        // Rotate continuous servo in one direction (forward) for 7 seconds
-        if ((curr_elapsed_time >= 6800) || (switch_state == HIGH)) {
-          curr_elapsed_time = 0;
-          move_servo_backward(motor1);
-        }
-
-        // Stop the servo
-        while ((switch_state == HIGH) && (curr_elapsed_time <= -6800)) {
-          mySwitch.loop();
-          switch_state = mySwitch.getState();
-          stop_servo(motor1);
-        }
-        motor1_pos = curr_elapsed_time;
+  switch (motor_state) {
+    case 0:
+      // stop_servo(motor_state, motor1);
+      // stop_servo(motor_state, motor2);
+      // stop_servo(motor_state, motor3);
+      // stop_servo(motor_state, motor4);
+      // stop_servo(motor_state, motor5);
+      // stop_servo(motor_state, motor6);
+      // setRGB(0, 0, 0); // Turn off the RGB LED
+      break;
+    case 1:
+      if ((motor_state == 1) && ((curr_elapsed_time <= -6800) || (switch_state == LOW))) {
         curr_elapsed_time = 0;
-        break;
-      case 2:
-        if ((curr_elapsed_time <= -6800) || (switch_state == LOW)) {
-          curr_elapsed_time = 0;
-          move_servo_forward(motor2);
-        }
-        // Stop the servo
-        while ((switch_state == LOW) && (curr_elapsed_time >= 6800)) {
-          mySwitch.loop();
-          switch_state = mySwitch.getState();
-          stop_servo(motor2);
-        }
-
-        // Rotate continuous servo in one direction (forward) for 7 seconds
-        if ((curr_elapsed_time >= 6800) || (switch_state == HIGH)) {
-          curr_elapsed_time = 0;
-          move_servo_backward(motor2);
-        }
-
-        // Stop the servo
-        while ((switch_state == HIGH) && (curr_elapsed_time <= -6800)) {
-          mySwitch.loop();
-          switch_state = mySwitch.getState();
-          stop_servo(motor2);
-        }
-        motor2_pos = curr_elapsed_time;
-        curr_elapsed_time = 0;
-        break;
-      case 3:
-        if ((curr_elapsed_time <= -6800) || (switch_state == LOW)) {
-          curr_elapsed_time = 0;
-          move_servo_forward(motor3);
-        }
-        // Stop the servo
-        while ((switch_state == LOW) && (curr_elapsed_time >= 6800)) {
-          mySwitch.loop();
-          switch_state = mySwitch.getState();
-          stop_servo(motor3);
-        }
-
-        // Rotate continuous servo in one direction (forward) for 7 seconds
-        if ((curr_elapsed_time >= 6800) || (switch_state == HIGH)) {
-          curr_elapsed_time = 0;
-          move_servo_backward(motor3);
-        }
-
-        // Stop the servo
-        while ((switch_state == HIGH) && (curr_elapsed_time <= -6800)) {
-          mySwitch.loop();
-          switch_state = mySwitch.getState();
-          stop_servo(motor3);
-        }
-        motor3_pos = curr_elapsed_time;
-        curr_elapsed_time = 0;
-        break;
-      case 4:
-        if ((curr_elapsed_time <= -6800) || (switch_state == LOW)) {
-          curr_elapsed_time = 0;
-          move_servo_forward(motor4);
-        }
-        // Stop the servo
-        while ((switch_state == LOW) && (curr_elapsed_time >= 6800)) {
-          mySwitch.loop();
-          switch_state = mySwitch.getState();
-          stop_servo(motor4);
-        }
-
-        // Rotate continuous servo in one direction (forward) for 7 seconds
-        if ((curr_elapsed_time >= 6800) || (switch_state == HIGH)) {
-          curr_elapsed_time = 0;
-          move_servo_backward(motor4);
-        }
-
-        // Stop the servo
-        while ((switch_state == HIGH) && (curr_elapsed_time <= -6800)) {
-          mySwitch.loop();
-          switch_state = mySwitch.getState();
-          stop_servo(motor4);
-        }
-        motor4_pos = curr_elapsed_time;
-        curr_elapsed_time = 0;
-        break;
-      case 5:
-        if ((curr_elapsed_time <= -6800) || (switch_state == LOW)) {
-          curr_elapsed_time = 0;
-          move_servo_forward(motor5);
-        }
-        // Stop the servo
-        while ((switch_state == LOW) && (curr_elapsed_time >= 6800)) {
-          mySwitch.loop();
-          switch_state = mySwitch.getState();
-          stop_servo(motor5);
-        }
-
-        // Rotate continuous servo in one direction (forward) for 7 seconds
-        if ((curr_elapsed_time >= 6800) || (switch_state == HIGH)) {
-          curr_elapsed_time = 0;
-          move_servo_backward(motor5);
-        }
-
-        // Stop the servo
-        while ((switch_state == HIGH) && (curr_elapsed_time <= -6800)) {
-          mySwitch.loop();
-          switch_state = mySwitch.getState();
-          stop_servo(motor5);
-        }
-        motor5_pos = curr_elapsed_time;
-        curr_elapsed_time = 0;
-        break;
-      case 6:
-        if ((curr_elapsed_time <= -6800) || (switch_state == LOW)) {
-          curr_elapsed_time = 0;
-          move_servo_forward(motor6);
-        }
-        // Stop the servo
-        while ((switch_state == LOW) && (curr_elapsed_time >= 6800)) {
-          mySwitch.loop();
-          switch_state = mySwitch.getState();
-          stop_servo(motor6);
-        }
-
-        // Rotate continuous servo in one direction (forward) for 7 seconds
-        if ((curr_elapsed_time >= 6800) || (switch_state == HIGH)) {
-          curr_elapsed_time = 0;
-          move_servo_backward(motor6);
-        }
-
-        // Stop the servo
-        while ((switch_state == HIGH) && (curr_elapsed_time <= -6800)) {
-          mySwitch.loop();
-          switch_state = mySwitch.getState();
-          stop_servo(motor6);
-        }
-        motor6_pos = curr_elapsed_time;
-        curr_elapsed_time = 0;
-        break;
-      case 7:
+        move_servo_forward(motor_state, motor1);
+      }
+      // Stop the servo
+      // Serial.print("finished moving forward");
+      // Serial.print(motor_state);
+      // Serial.print(curr_elapsed_time);
+      while ((motor_state == 1) && (switch_state == LOW) && (curr_elapsed_time >= 6800)) {
         mySwitch.loop();
         switch_state = mySwitch.getState();
-        if (switch_state == HIGH) {
-          setRGB(255, 0, 0); // Set RGB LED to red
-        } else {
-          setRGB(0, 0, 0); // Turn off the RGB LED
-        }
-        led_state_pos = digitalRead(ledRedPin) ? 10 : 1;
-        break;
-      case 8:
-        Serial.print("e:");
-        Serial.println(motor1_pos);
+        stop_servo(motor_state, motor1);
+      }
 
-        Serial.print("n:");
-        Serial.println(motor2_pos);
+      // Rotate continuous servo in one direction (forward) for 7 seconds
+      //Serial.print(motor1_pos);
+      if ((motor_state == 1) && ((curr_elapsed_time >= 6800) || (switch_state == HIGH))) {
+        curr_elapsed_time = 0;
+        move_servo_backward(motor_state, motor1);
+      }
 
-        Serial.print("m:");
-        Serial.println(motor3_pos);
+      // Stop the servo
+      // Serial.print("finished moving backward");
+      // Serial.print(motor_state);
+      // Serial.print(curr_elapsed_time);
+      while ((motor_state == 1) && (switch_state == HIGH) && (curr_elapsed_time <= -6800)) {
+        mySwitch.loop();
+        switch_state = mySwitch.getState();
+        stop_servo(motor_state, motor1);
+      }
+      motor1.write(90);   // stop the motor
+      motor1_pos = curr_elapsed_time;
+      curr_elapsed_time = 0;
+      break;
+    case 2:
+      if ((motor_state == 2) && ((curr_elapsed_time <= -6800) || (switch_state == LOW))) {
+        curr_elapsed_time = 0;
+        move_servo_forward(motor_state, motor2);
+      }
+      // Stop the servo
+      //Serial.print(motor2_pos);
+      while ((motor_state == 2) && (switch_state == LOW) && (curr_elapsed_time >= 6800)) {
+        mySwitch.loop();
+        switch_state = mySwitch.getState();
+        stop_servo(motor_state, motor2);
+      }
 
-        Serial.print("t:");
-        Serial.println(motor4_pos);
+      // Rotate continuous servo in one direction (forward) for 7 seconds
+      if ((motor_state == 2) && ((curr_elapsed_time >= 6800) || (switch_state == HIGH))) {
+        curr_elapsed_time = 0;
+        move_servo_backward(motor_state, motor2);
+      }
 
-        Serial.print("u:");
-        Serial.println(motor5_pos);
+      // Stop the servo
+      //Serial.print(motor2_pos);
+      while ((motor_state == 2) && (switch_state == HIGH) && (curr_elapsed_time <= -6800)) {
+        mySwitch.loop();
+        switch_state = mySwitch.getState();
+        stop_servo(motor_state, motor2);
+      }
+      motor2.write(90);   // stop the motor
+      motor2_pos = curr_elapsed_time;
+      curr_elapsed_time = 0;
+      break;
+    case 3:
+      if ((motor_state == 3) && ((curr_elapsed_time <= -6800) || (switch_state == LOW))) {
+        curr_elapsed_time = 0;
+        move_servo_forward(motor_state, motor3);
+      }
+      // Stop the servo
+      while ((motor_state == 3) && (switch_state == LOW) && (curr_elapsed_time >= 6800)) {
+        mySwitch.loop();
+        switch_state = mySwitch.getState();
+        stop_servo(motor_state, motor3);
+      }
 
-        Serial.print("r:");
-        Serial.println(motor6_pos);
+      // Rotate continuous servo in one direction (forward) for 7 seconds
+      if ((motor_state == 3) && ((curr_elapsed_time >= 6800) || (switch_state == HIGH))) {
+        curr_elapsed_time = 0;
+        move_servo_backward(motor_state, motor3);
+      }
 
-        Serial.print("j:");
-        Serial.println(led_state_pos);
+      // Stop the servo
+      while ((motor_state == 3) && (switch_state == HIGH) && (curr_elapsed_time <= -6800)) {
+        mySwitch.loop();
+        switch_state = mySwitch.getState();
+        stop_servo(motor_state, motor3);
+      }
+      motor3.write(90);   // stop the motor
+      motor3_pos = curr_elapsed_time;
+      curr_elapsed_time = 0;
+      break;
+    case 4:
+      if ((motor_state == 4) && ((curr_elapsed_time <= -6800) || (switch_state == LOW))) {
+        curr_elapsed_time = 0;
+        move_servo_forward(motor_state, motor4);
+      }
+      // Stop the servo
+      while ((motor_state == 4) && (switch_state == LOW) && (curr_elapsed_time >= 6800)) {
+        mySwitch.loop();
+        switch_state = mySwitch.getState();
+        stop_servo(motor_state, motor4);
+      }
 
-        break;
-    }
+      // Rotate continuous servo in one direction (forward) for 7 seconds
+      if ((motor_state == 4) && ((curr_elapsed_time >= 6800) || (switch_state == HIGH))) {
+        curr_elapsed_time = 0;
+        move_servo_backward(motor_state, motor4);
+      }
+
+      // Stop the servo
+      while ((motor_state == 4) && (switch_state == HIGH) && (curr_elapsed_time <= -6800)) {
+        mySwitch.loop();
+        switch_state = mySwitch.getState();
+        stop_servo(motor_state, motor4);
+      }
+      motor4.write(90);   // stop the motor
+      motor4_pos = curr_elapsed_time;
+      curr_elapsed_time = 0;
+      break;
+    case 5:
+      if ((motor_state == 5) && ((curr_elapsed_time <= -6800) || (switch_state == LOW))) {
+        curr_elapsed_time = 0;
+        move_servo_forward(motor_state, motor5);
+      }
+      // Stop the servo
+      while ((motor_state == 5) && (switch_state == LOW) && (curr_elapsed_time >= 6800)) {
+        mySwitch.loop();
+        switch_state = mySwitch.getState();
+        stop_servo(motor_state, motor5);
+      }
+
+      // Rotate continuous servo in one direction (forward) for 7 seconds
+      if ((motor_state == 5) && ((curr_elapsed_time >= 6800) || (switch_state == HIGH))) {
+        curr_elapsed_time = 0;
+        move_servo_backward(motor_state, motor5);
+      }
+
+      // Stop the servo
+      while ((motor_state == 5) && (switch_state == HIGH) && (curr_elapsed_time <= -6800)) {
+        mySwitch.loop();
+        switch_state = mySwitch.getState();
+        stop_servo(motor_state, motor5);
+      }
+      motor5.write(90);   // stop the motor
+      motor5_pos = curr_elapsed_time;
+      curr_elapsed_time = 0;
+      break;
+    case 6:
+      if ((motor_state == 6) && ((curr_elapsed_time <= -6800) || (switch_state == LOW))) {
+        curr_elapsed_time = 0;
+        move_servo_forward(motor_state, motor6);
+      }
+      // Stop the servo
+      while ((motor_state == 6) && (switch_state == LOW) && (curr_elapsed_time >= 6800)) {
+        mySwitch.loop();
+        switch_state = mySwitch.getState();
+        stop_servo(motor_state, motor6);
+      }
+
+      // Rotate continuous servo in one direction (forward) for 7 seconds
+      if ((motor_state == 6) && ((curr_elapsed_time >= 6800) || (switch_state == HIGH))) {
+        curr_elapsed_time = 0;
+        move_servo_backward(motor_state, motor6);
+      }
+
+      // Stop the servo
+      while ((motor_state == 6) && (switch_state == HIGH) && (curr_elapsed_time <= -6800)) {
+        mySwitch.loop();
+        switch_state = mySwitch.getState();
+        stop_servo(motor_state, motor6);
+      }
+      motor6.write(90);   // stop the motor
+      motor6_pos = curr_elapsed_time;
+      curr_elapsed_time = 0;
+      break;
+    case 7:
+      motor6.write(90);
+      mySwitch.loop();
+      switch_state = mySwitch.getState();
+      if (switch_state == HIGH) {
+        setRGB(255, 0, 0); // Set RGB LED to red
+      } else {
+        setRGB(0, 0, 0); // Turn off the RGB LED
+      }
+      
+      led_state_pos = digitalRead(ledRedPin) ? 1 : 10;
+      //setRGB(0, 0, 0); // Turn off the RGB LED
+      //led_state_pos = 0;
+      break;
+    case 8:
+      // first stop all servos and LED
+      // stop_servo(motor_state, motor1);
+      // stop_servo(motor_state, motor2);
+      // stop_servo(motor_state, motor3);
+      // stop_servo(motor_state, motor4);
+      // stop_servo(motor_state, motor5);
+      // stop_servo(motor_state, motor6);
+      // setRGB(0, 0, 0); // Turn off the RGB LED
+
+      Serial.print("e:");
+      Serial.println(motor1_pos);
+
+      Serial.print("n:");
+      Serial.println(motor2_pos);
+
+      Serial.print("m:");
+      Serial.println(motor3_pos);
+
+      Serial.print("t:");
+      Serial.println(motor4_pos);
+
+      Serial.print("u:");
+      Serial.println(motor5_pos);
+
+      Serial.print("r:");
+      Serial.println(motor6_pos);
+
+      Serial.print("j:");
+      Serial.println(led_state_pos);
+
+      break;
+  }
 
     delay(200);
-  }
 }
