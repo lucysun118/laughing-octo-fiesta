3e14591854970eb358f4fc856d6360029d7cedde
Richard Corrente <rc@Richards-MacBook-Air-2.local>
2023-12-18 21:52:25 -0500

    updated serial code to only make openai calls

--- serial_read/test_serialread.py
+++ serial_read/test_serialread.py
@@ -22,47 +22,52 @@ def read_line():
 
 while True:
     elapsed_time_val = read_line()
-    print(f"raw val: {elapsed_time_val}")
+    if elapsed_time_val == "":
+        continue
+    else:
+        print(f"scaled val: {scale_values(int(elapsed_time_val[2:]))}")
+        scaled_val = scale_values(int(elapsed_time_val[2:]))
 
-    # Check the first two characters of the incoming serial data
-    prefix = elapsed_time_val[:2]
+        # Check the first two characters of the incoming serial data
+        prefix = elapsed_time_val[:2]
 
-    # Assign values based on the prefix
-    if prefix == "e:":
-        e = int(elapsed_time_val[2:])
-    elif prefix == "n:":
-        n = int(elapsed_time_val[2:])
-    elif prefix == "m:":
-        m = int(elapsed_time_val[2:])
-    elif prefix == "t:":
-        t = int(elapsed_time_val[2:])
-    elif prefix == "u:":
-        u = int(elapsed_time_val[2:])
-    elif prefix == "r:":
-        r = int(elapsed_time_val[2:])
-    elif prefix == "j:":
-        j = int(elapsed_time_val[2:])
+        # Assign values based on the prefix
+        if prefix == "e:":
+            e = scaled_val
+        elif prefix == "n:":
+            n = scaled_val
+        elif prefix == "m:":
+            m = scaled_val 
+        elif prefix == "t:":
+            t = scaled_val 
+        elif prefix == "u:":
+            u = scaled_val 
+        elif prefix == "r:":
+            r = scaled_val 
+        elif prefix == "j:":
+            j = scaled_val
 
-    print(f"e: {e}, n: {n}, m: {m}, t: {t}, u: {u}, r: {r}, j: {j}")
 
-    # Check if all variables are set
-    if all((e, n, m, t, u, r, j)):
-        # Run the command with the stored values
-        command = ["/Users/lucysun/laughing-octo-fiesta/serial_read/musenet_code/text_to_midi.py",
-                   "-e", str(e), "-n", str(n), "-m", str(m), "-t", str(t), "-u", str(u), "-r", str(r), "-j", str(j)]
+        print(f"e: {e}, n: {n}, m: {m}, t: {t}, u: {u}, r: {r}, j: {j}")
 
-        subprocess.run(["/usr/local/bin/python3"] + command)
+        # Check if all variables are set
+        if  (e !=0  and n != 0 and m != 0 and t != 0 and u != 0 and r != 0 and j != 0) and not command_executed:
+            # Run the command with the stored values
+            command = ["/Users/rc/laughing-octo-fiesta/serial_read/musenet_code/text_to_midi.py",
+                    "-e", str(e), "-n", str(n), "-m", str(m), "-t", str(t), "-u", str(u), "-r", str(r), "-j", str(j)]
 
-        # Open the HTML page with the generated MIDI file in a web browser
-        webbrowser.open('midi_notes.html', new=2)
+            subprocess.run(["python3"] + command)
 
-        # Reset variables
-        e, n, m, t, u, r, j = 0, 0, 0, 0, 0, 0, 0
-        command_executed = True
+            # Open the HTML page with the generated MIDI file in a web browser
+            webbrowser.open('midi_notes.html', new=2)
 
-    elif command_executed:
-        # Reset variables if the command has been executed and not all variables are set
-        e, n, m, t, u, r, j = 0, 0, 0, 0, 0, 0, 0
-        command_executed = False
+            # Reset variables
+            e, n, m, t, u, r, j = 0, 0, 0, 0, 0, 0, 0
+            #command_executed = True
+
+        # else:
+        #     # Reset variables if the command has been executed and not all variables are set
+        #     #e, n, m, t, u, r, j = 0, 0, 0, 0, 0, 0, 0
+        #     command_executed = False
 
 ser.close()
